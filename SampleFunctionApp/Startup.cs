44779fc501eef14a7664a62e927482ad6146ffc5
george2006 <durbich@outlook.com>
2022-02-26 08:42:47 +0100

    Changes to make code run
    Changes needed to make code run

--- SampleFunctionApp/Startup.cs
+++ SampleFunctionApp/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using OidcApiAuthorization;
 
 [assembly: FunctionsStartup(typeof(SampleFunctionApp.Startup))]
@@ -9,6 +11,16 @@ namespace SampleFunctionApp
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddOidcApiAuthorization();
+        }
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+            FunctionsHostBuilderContext context = builder.GetContext();
+            builder.ConfigurationBuilder
+                    .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                    .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"local.settings.json"), optional: true, reloadOnChange: false)
+                    .AddEnvironmentVariables();
+
         }
     }
 }
